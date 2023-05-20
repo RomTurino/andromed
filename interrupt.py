6836c0c6158c6c6aba9d2600d380fe71886738ec
RomTurino <73258691+RomTurino@users.noreply.github.com>
2023-05-20 19:36:48 +0300

    add hour

--- interrupt.py
+++ interrupt.py
@@ -3,10 +3,9 @@ from telegram.ext import CallbackContext, ConversationHandler
 from stickers import *
 
 
-
 def end(update: Update, context: CallbackContext):
     update.message.reply_photo(
-        "https://myslide.ru/documents_7/c2d573d9e648a68842706b978ad351da/img17.jpg", 
+        "https://myslide.ru/documents_7/c2d573d9e648a68842706b978ad351da/img17.jpg",
         reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
@@ -14,3 +13,40 @@ def end(update: Update, context: CallbackContext):
 def wrong_message(update: Update, context: CallbackContext):
     update.message.reply_sticker(WRONG_COMMAND_STICKER)
     update.message.reply_text('Упс! Такой команды не существует')
+
+
+def endpoint(update: Update, context: CallbackContext):
+    update.message.reply_sticker(ENDPOINT_STICKER)
+    update.message.reply_text('Операция прервана')
+    return ConversationHandler.END  # завершает диалог о добавлении дела
+
+
+def delete_message(update: Update, context: CallbackContext, start=0, end=1):
+    try:
+        for i in range(start, end):
+            context.bot.delete_message(update.effective_chat.id,
+                                       update.effective_message.message_id - i)  # i = 0, 1, 2, 3, 4
+    except:  # если поймаешь ошибки
+        pass  # pass
+
+
+
+
+# fruits = [
+#     ["Банан", "Яблоко", "Лимон"],
+#     ["Банан", "Яблоко", "Лимон"],
+#     ["Банан", "Яблоко", "Лимон"]
+# ]  # список
+# word = "Спидометр"
+# prices = {"Симиренко": 74, "Айдаред": 104}
+# numbers = range(10)  # диапазон
+
+# for name, price in prices.items():
+#     print(f'Цена яблок {name} = {price} рублей', end=" ")
+# print()
+# for banana, apple, lemon in fruits:
+#     print(banana)
+
+
+# a, b, c = [1, 2, 3]
+# print(a)
--- task_create.py
+++ task_create.py
@@ -7,46 +7,79 @@ from telegram.ext import (
     CallbackContext,
     CallbackQueryHandler
 )
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from constants import *
-from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from interrupt import *
+from telegram_bot_calendar import DetailedTelegramCalendar
 from datetime import date
 
 
 
+class MyStyleCalendar(DetailedTelegramCalendar):
+    # previous and next buttons style. they are emoji now!
+    prev_button = "⬅️"
+    next_button = "➡️"
+    # you do not want empty cells when month and year are being selected
+    empty_month_button = ""
+    empty_year_button = ""
+    middle_button_year = ""
 
 
 def add_task(update: Update, context: CallbackContext):
     name = update.effective_user.first_name
+    context.user_data['start_delete'] = update.effective_message.message_id
     update.message.reply_sticker(ADD_STICKER)
     update.message.reply_text(f"Просьба ввести текст дела, мастер {name} или /no чтобы прекратить операцию добавления")
     return TASK
 
 def handle_task_text(update: Update, context: CallbackContext):
+    
     message = update.message.text # взяли сообщение, где пользователь пишет текст дела
     context.user_data["todo_text"] = message # сохранили это в рюкзак
     # update.message.reply_text(message)
-    calendar, step = DetailedTelegramCalendar(locale="ru", min_date=date.today()).build()
+    calendar, step = MyStyleCalendar(locale="ru", min_date=date.today()).build()
     context.bot.send_message(update.effective_chat.id,
                      f"Выберите {RU_STEP[step]}",
                      reply_markup=calendar)
     return DATE
 
 def handle_date(update: Update, context: CallbackContext):
-    result, key, step = DetailedTelegramCalendar(locale="ru", min_date=date.today()).process(update.callback_query.data)
+    result, key, step = MyStyleCalendar(locale="ru", min_date=date.today()).process(update.callback_query.data)
     if not result and key:
         context.bot.send_message(update.effective_chat.id,
                                  f"Выберите {RU_STEP[step]}", reply_markup=key)
     elif result:
+        delete_message(update, context, end=3)
+        year, month, day = str(result).split('-')
+        true_date = day + "." + month + "." + year
         context.bot.send_message(update.effective_chat.id,
-                                 f"Вы выбрали {result}")
-        
+                                 f"Вы выбрали {true_date}")
+        context.user_data["date"] = true_date # сохранили дату в рюкзак 
+        return HOUR
     
+    
+def handle_hour(update: Update, context: CallbackContext):
+    keyboard = []
+    steps = {1: 0,
+             2: 6,
+             3: 12,
+             4: 18}
+    for line in range(4):
+        keyboard.append([])
+        for column in range(6):
+            keyboard[line].append(InlineKeyboardButton(text=f"{column} + {steps[line-1]}",
+                                                       callback_data=f"{column} + {steps[line-1]}"))
+    markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Выбери час к которому нужно дело завершить",
+        reply_markup=markup,
+    )
+    
+            
 
-def endpoint(update: Update, context: CallbackContext):
-    update.message.reply_sticker(ENDPOINT_STICKER)
-    update.message.reply_text('Операция прервана')
-    return ConversationHandler.END # завершает диалог о добавлении дела
+
+
 
 
 add_handler = ConversationHandler(#обработчик диалога
